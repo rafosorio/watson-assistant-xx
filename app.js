0517ef6d6d95ec4a1723be40ec014ce9d7b2b24e
Rafael Osorio <rafosorio@gmail.com>
2019-02-09 12:11:53 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2015 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var express = require('express'); // app server
+var bodyParser = require('body-parser'); // parser for post requests
+var AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
+var request = require("request");
+var requestjs = require("request-json");
+
+var workspace = 'bf??';
+var assitantUsername = 'apikey';
+var assistantPassword = "Gu_??";
+
+var app = express();
+
+// Bootstrap application settings
+app.use(express.static('./public')); // load UI from public folder
+app.use(bodyParser.json());
+
+// Create the service wrapper
+
+var assistant = new AssistantV1({
+  version: '2018-07-10',
+  username: assitantUsername,
+  password: assistantPassword
+});
+
+// Endpoint to be call from the client side
+app.post('/api/message', function (req, res) {
+  if (!workspace || workspace === '<workspace-id>') {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/assistant-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  var payload = {
+    workspace_id: workspace,
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+
+  // Send the input to the assistant service
+  assistant.message(payload, function (err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Assistant service
+ * @param  {Object} response The response from the Assistant service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var newResp;
+  if (!response.output) {
+    response.output = {};
+  }
+
+  return response;
+
+}
+
+module.exports = app;
